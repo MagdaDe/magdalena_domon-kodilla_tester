61c951d884f3f0a57d524b52afca90477eece021
Magdalena Domon <magdalena1983@gmail.com>
2020-01-22 16:40:48 +0100

    Moduł 2.10 zadanie

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -35,5 +35,5 @@ public class FirstClass {
         afterLeaseNotebook.checkPrice();
         afterLeaseNotebook.checkWeight();
         afterLeaseNotebook.checkPriceToYear();
-    }
+            }
 }
